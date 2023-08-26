b011aef2806ea468ae89b8eb537b044aa113e67b
Tao Wei <taowei@buffalo.edu>
2023-08-25 23:25:16 -0700

    update app_tmpl.py

--- app_tmpl.py
+++ app_tmpl.py
@@ -1,14 +1,14 @@
 # app.py : Multimodal Chatbot
-import gradio as gr
-import random
+import logging
 from pprint import pprint
 from dotenv import load_dotenv
+import gradio as gr
 
-from utils import parse_message, format_to_message
+################################################################
+# Load .env and logging
+################################################################
 
 load_dotenv()  # take environment variables from .env.
-
-import logging
 
 logging.basicConfig(level=logging.WARN, format='%(asctime)-15s] %(message)s', datefmt="%m/%d/%Y %I:%M:%S %p %Z")
 
@@ -16,8 +16,9 @@ def print(*args, **kwargs):
     sep = kwargs['sep'] if 'sep' in kwargs else ' '
     logging.warning(sep.join([str(val) for val in args])) # use level WARN for print, as gradio level INFO print unwanted messages
 
-
-#################################################################################
+################################################################
+# Global variables
+################################################################
 
 TITLE = "Multimodal Chatbot Template"
 
@@ -37,17 +38,20 @@ ATTACHMENTS = {
 }
 
 SETTINGS = {
-    'chat_engine': dict(cls='Radio', choices=['auto', 'random', 'openai'], value='auto', 
+    'session_state': dict(cls='State', value={}),
+    'chat_engine': dict(cls='Radio', choices=['auto', 'random', 'openai', 'openai_stream'], value='auto', 
             interactive=True, label="Chat engine"),
     'system_prompt': dict(cls='Textbox', interactive=True, lines=5, label="System prompt"),
 }
 
 PARAMETERS = {
 }
-
-KWARGS = {}
-
-#################################################################################
+    
+KWARGS = {} # use for chatbot additional_inputs, do NOT change
+    
+################################################################
+# utils
+################################################################
 
 def _create_from_dict(PARAMS):
     params = {}
@@ -56,75 +60,21 @@ def _create_from_dict(PARAMS):
         params[name] = getattr(gr, cls_)(**kwargs)
     return params
 
-def _random_bot_fn(message, history, **kwargs):
-    # Example multimodal messages
-    samples = [
-        format_to_message(dict(text="I love cat", images=["https://upload.wikimedia.org/wikipedia/commons/2/25/Siam_lilacpoint.jpg"])),
-        format_to_message(dict(text="I hate cat", images=["https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Felis_catus-cat_on_snow.jpg/2560px-Felis_catus-cat_on_snow.jpg"])),
-        format_to_message(dict(audios=["https://upload.wikimedia.org/wikipedia/commons/2/28/Caldhu.wav"])),
-        format_to_message(dict(videos=["https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/360/Big_Buck_Bunny_360_10s_1MB.mp4"])),
-        format_to_message(dict(files=["https://www.africau.edu/images/default/sample.pdf"])),
-        format_to_message(dict(text="Hello, how can I assist you today?", buttons=['Primary', dict(text='Secondary', value="the second choice")])),
-        format_to_message(dict(text="We found the following items:", cards=[
-            dict(image="https://upload.wikimedia.org/wikipedia/commons/2/25/Siam_lilacpoint.jpg", title="Siam Lilac Point", 
-                 text="The lilac point Siamese cat usually has a pale pink nose and pale pink paw pads, with silver-gray fur surrounding those points."),
-            dict(image="https://upload.wikimedia.org/wikipedia/commons/2/25/Siam_lilacpoint.jpg", 
-                 title="Siam Lilac Point", text="The lilac point Siamese cat usually has a pale pink nose and pale pink paw pads, with silver-gray fur surrounding those points.",
-                 extra="""<a href="https://upload.wikimedia.org/wikipedia/commons/2/25/Siam_lilacpoint.jpg" class="btn btn-primary btn-sm text-white">More</a>"""),
-        ])),
-    ]
-    if 'pdf' in message:
-        bot_message = samples[4]
-    elif 'button' in message:
-        bot_message = samples[5]
-    elif 'card' in message:
-        bot_message = samples[6]
-    else:
-        bot_message = random.choice(samples)
-    return bot_message
 
-def _openai_bot_fn(message, history, **kwargs):
-    import openai, os
-    openai.api_key = os.environ["OPENAI_API_KEY"]
+from app_tmpl_bot_fn import _random_bot_fn, _openai_bot_fn, _openai_stream_bot_fn
 
-    resp = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-                {"role": "user", "content": message},
-            ],
-    )
-    return resp.choices[0].message.content
-
-def _openai_bot_stream_fn(message, history, **kwargs):
-    import openai, os
-    openai.api_key = os.environ["OPENAI_API_KEY"]
-
-    resp = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-                {"role": "user", "content": message},
-            ],
-        stream=True,
-    )
-
-    bot_message = ""
-    for _resp in resp:
-        if 'content' in _resp.choices[0].delta: # last resp delta is empty
-            bot_message += _resp.choices[0].delta.content # need to accumulate previous message
-        yield bot_message.strip() # accumulated message can easily be postprocessed
-
-def bot_fn(message, history, *args):
+def _bot_fn(message, history, *args):
     kwargs = {name: value for name, value in zip(KWARGS.keys(), args)}
 
     kwargs['chat_engine'] = 'random' if kwargs['chat_engine'] == 'auto' else kwargs['chat_engine']
 
     bot_message = {'random': _random_bot_fn,
-        'openai': _openai_bot_stream_fn,
+        'openai': _openai_bot_fn,
+        'openai_stream': _openai_stream_bot_fn,
         }.get(kwargs['chat_engine'])(message, history, **kwargs)
     
     if isinstance(bot_message, str):
-        for i in range(len(bot_message)):
-            yield bot_message[:i+1]
+        yield bot_message
     else:
         for m in bot_message:
             yield m
@@ -132,6 +82,32 @@ def bot_fn(message, history, *args):
     print(kwargs)
     pprint(history + [[message, bot_message]])
 
+def _bot_fn_session_state(message, history, *args):
+    kwargs = {name: value for name, value in zip(KWARGS.keys(), args)}
+
+    session_state = kwargs['session_state']
+    kwargs['chat_engine'] = 'random' if kwargs['chat_engine'] == 'auto' else kwargs['chat_engine']
+
+    bot_message = {'random': _random_bot_fn,
+        'openai': _openai_bot_fn,
+        'openai_stream': _openai_stream_bot_fn,
+        }.get(kwargs['chat_engine'])(message, history, **kwargs)
+    
+    if isinstance(bot_message, str):
+        yield bot_message, session_state
+    else:
+        for m in bot_message:
+            yield m, session_state
+
+    print(kwargs)
+    pprint(history + [[message, bot_message]])
+    session_state['previous_message'] = message
+
+bot_fn = _bot_fn_session_state if 'session_state' in SETTINGS else _bot_fn
+
+################################################################
+# Gradio app
+################################################################
 
 def get_demo():
 
@@ -141,14 +117,17 @@ min-height: 600px;
 }"""
 
     # NOTE: can not be inside another gr.Blocks
-    # _chatbot = gr.Chatbot(elem_id="chatbot", avatar_images = ("user.png", "bot.png"))
+    # _chatbot = gr.Chatbot(elem_id="chatbot", avatar_images = ("assets/user.png", "assets/bot.png"))
     # _textbox = gr.Textbox(container=False, show_label=False, placeholder="Type a message...", scale=10, elem_id='inputTextBox', min_width=300)
 
     with gr.Blocks(css=css) as demo:
+        # title
         gr.HTML(f"<center><h1>{TITLE}</h1></center>")
+        # description
         with gr.Accordion("Expand to see Introduction and Usage", open=False):
             gr.Markdown(f"{DESCRIPTION}")
         with gr.Row():
+            # attachements, settings, and parameters
             with gr.Column(scale=1):
                 attachments = _create_from_dict(ATTACHMENTS)
                 with gr.Accordion("Settings", open=False) as settings_accordin:
@@ -157,14 +136,17 @@ min-height: 600px;
                     parameters = _create_from_dict(PARAMETERS)
 
             with gr.Column(scale=9):
+                # chatbot
                 global KWARGS
                 KWARGS = {**attachments, **settings, **parameters}
                 import chat_interface
                 chatbot = chat_interface.ChatInterface(bot_fn, # chatbot=_chatbot, textbox=_textbox,
                         additional_inputs=list(KWARGS.values()),
+                        additional_outputs=[KWARGS['session_state']] if 'session_state' in KWARGS else None,
                         retry_btn="Retry", undo_btn="Undo", clear_btn="Clear",
                     )
 
+                # examples
                 with gr.Accordion("Examples", open=False) as examples_accordin:
                     chat_examples = gr.Examples(
                         ["What's the Everett interpretation of quantum mechanics?",
@@ -173,7 +155,7 @@ min-height: 600px;
                         ],
                         inputs=chatbot.textbox, label="AI Chat Examples",
                     )
-
+            # additional handlers
             if hasattr(chatbot, '_upload_fn'):
                 for name, attach in attachments.items():
                     attach.change(chatbot._upload_fn,
