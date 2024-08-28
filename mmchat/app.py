88deddfd846dc4f5ab6cdd5fd5019c684a30ba66
Tao Wei <taowei@buffalo.edu>
2024-08-28 01:44:26 -0700

    settings

--- mmchat/app.py
+++ mmchat/app.py
@@ -24,8 +24,65 @@ from utils import llms
 llms.print = print
 
 ################################################################
+# Globals
+################################################################
+
+_default_session_state = dict(
+        context_switch_at=0, # history before context_switch_at should be ignored (e.g. upload an image or a file)
+        message=None,
+        previous_message=None,
+    )
+
+TITLE = """ Gradio Multimodal Chatbot Template """
+
+DESCRIPTION = """Welcome
+"""
+
+SETTINGS = {
+    'Info': {
+        '__metadata': {'open': False, 'tabbed': True},
+        'image': dict(cls='Image', type='filepath', label="Input"), #, source='webcam'),
+        'system_prompt': dict(cls='Textbox', interactive=True, lines=5, label="System prompt"),
+        'status': dict(cls='JSON', label='Status'),
+    },
+    'Settings': {
+        '__metadata': {'open': False, 'tabbed': False},
+        'session_state': dict(cls='State', value=_default_session_state),
+        'chat_engine': dict(cls='Radio', choices=['auto', 'random', 'gpt-3.5-turbo'], value='auto', 
+                interactive=True, label="Chat engine"),
+        'speech_synthesis': dict(cls='Checkbox', value=False, 
+                interactive=True, label="Speech Synthesis"),
+    },
+    'Parameters': {
+        '__metadata': {'open': True, 'tabbed': False},
+        'temperature': dict(cls='Slider', minimum=0, maximum=1, value=0.7, step=0.1, interactive=True, label="Temperature")
+    }
+}
+
+# SETTINGS = {}
+
+################################################################
 # Utils
 ################################################################
+
+def _create_from_dict(PARAMS, tabbed=False):
+    params = {}
+    for name, kwargs in PARAMS.items():
+        if name.startswith('__'):
+            continue
+        cls_ = kwargs['cls']; del kwargs['cls']
+        if not tabbed:
+            params[name] = getattr(gr, cls_)(**kwargs)
+        else:
+            tab_name = kwargs['label'] if 'label' in kwargs else name
+            with gr.Tab(tab_name):
+                params[name] = getattr(gr, cls_)(**kwargs)
+    return params
+
+def _clear(session_state):
+    session_state.clear()
+    session_state.update(_default_session_state)
+    return session_state
 
 def transcribe(audio=None):
     try:
@@ -55,11 +112,25 @@ def get_demo():
     padding-right: 8px;
     }"""
     with gr.Blocks(css=css) as demo:
-        from utils.gradio import ChatInterface
-        chatbot = ChatInterface(bot_fn, type='messages', 
-                multimodal=False,
-                avatar_images=('assets/user.png', 'assets/bot.png'))
-        chatbot.audio_btn.click(transcribe, [], [chatbot.textbox], queue=False, api_name=False)
+        # title
+        gr.HTML(f"<center><h1>{TITLE}</h1></center>")
+        # description
+        with gr.Accordion("Expand to see Introduction and Usage", open=False):
+            gr.Markdown(f"{DESCRIPTION}")
+        with gr.Row():
+            with gr.Column(scale=1):
+                # settings
+                for section_name, _settings in SETTINGS.items():
+                    metadata = _settings['__metadata']
+                    with gr.Accordion(section_name, open=metadata.get('open', False)):
+                        settings = _create_from_dict(_settings, tabbed=metadata.get('tabbed', False))
+            with gr.Column(scale=9):
+                # chatbot
+                from utils.gradio import ChatInterface
+                chatbot = ChatInterface(bot_fn, type='messages', 
+                        multimodal=False,
+                        avatar_images=('assets/user.png', 'assets/bot.png'))
+                chatbot.audio_btn.click(transcribe, [], [chatbot.textbox], queue=False, api_name=False)
     return demo
 
 def parse_args():
